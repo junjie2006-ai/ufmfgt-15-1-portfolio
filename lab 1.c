a2e5a545b1a5ad68c50d3c0534e3dc365e39d52f
j75-wang <junjie2006-ai>
2026-03-12 17:31:42 +0000

    lab1

---
+++ lab 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int is_even_that(int a) {
+    return (a / 2);
+}
+
+
+int main()  {
+
+    int a ;
+
+    printf("Enter one integer:");
+    scanf("%d", &a);
+
+    printf("")
+
+    return 0;
+}
+//
+// Created by j75-wang on 12/03/2026.
+//
+
