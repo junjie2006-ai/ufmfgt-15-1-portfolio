a2f1335821f22b8bc39dcf712916315ef77d762b
junjie <junjie2006wang@gmial.com>
2026-03-24 19:21:12 +0000

    lab3 part2

--- lab3 .c
+++ lab3 .c
@@ -1,45 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int score;
+    float a,b,c,d,temp;
 
-    printf("Enter the signal strength (0-100): ");
-    scanf("%d", &score);
+    printf("Enter four numbers:\n");
+    scanf("%f %f %f %f", &a, &b, &c, &d);
 
-    // First, validate the input is within the 0-100 range
-    if (score < 0 || score > 100) {
-        printf("Error: Input outside 0-100 range.\n");
-    } else {
-        // Divide by 10 to turn ranges into single digits
-        switch (score / 10) {
-            case 10: // Special case for exactly 100
-            case 9:  // 90-99
-            case 8:  // 80-89
-                printf("Grade: A\n");
-                break;
+    if (a > b) { temp = a; a = b; b = temp; }
+    if (a > c) { temp = a; a = c; c = temp; }
+    if (a > d) { temp = a; a = d; d = temp; }
 
-            case 7:  // 70-79
-            case 6:  // 60-69
-                printf("Grade: B\n");
-                break;
+    if (b > c) { temp = b; b = c; c = temp; }
+    if (b > d) { temp = b; b = d; d = temp; }
 
-            case 5:  // 50-59
-            case 4:  // 40-49
-                printf("Grade: C\n");
-                break;
+    if (c > d) { temp = c; c = d; d = temp; }
 
-            case 3:  // 30-39
-                printf("Grade: D\n");
-                break;
 
-            default: // 0-29 (Covers cases 2, 1, and 0)
-                printf("Grade: F\n");
-                break;
-        }
-    }
+    printf("\nAscending order: %.2f %.2f %.2f %.2f\n", a, b, c, d);
+
+
+    printf("Descending order: %.2f %.2f %.2f %.2f\n", d, c, b, a);
 
     return 0;
 }
-
 
 
